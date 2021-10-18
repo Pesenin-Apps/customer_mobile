0c234cc59612e94331cd8a6fad3ae337475799f8
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-10-18 23:21:17 +0800

    add theme file

---
+++ lib/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = const Color(0xffFFA700);
+Color secondaryColor = const Color(0xff00A170);
+Color tertiaryColor = const Color(0xff8D887D);
+Color alertColor = const Color(0xffED6363);
+Color priceColor = const Color(0xff2C96F1 );
+Color backgraundColor1 = const Color(0xff252119);
+Color backgraundColor2 = const Color(0xff322D25);
+Color backgraundColor3 = const Color(0xff2F2A21);
+Color backgraundColor4 = const Color(0xff353025);
+Color primaryTextColor = const Color(0xffF1F0F2);
+Color secondaryTextColor = const Color(0xff9B9B9A);
+Color subtitleTextColor = const Color(0xff504F5E);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor
+);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleTextColor
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
