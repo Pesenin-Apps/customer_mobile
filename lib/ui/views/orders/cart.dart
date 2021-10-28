e31f889ffee46a84741c3196ba3833173a6a5c28
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-10-28 23:09:14 +0800

    add widget emptyCart

--- lib/ui/views/orders/cart.dart
+++ lib/ui/views/orders/cart.dart
@@ -32,7 +32,7 @@ class _CartState extends State<Cart> {
     Widget customerInfo() {
       return Container(
         margin: const EdgeInsets.only(top: 20.0),
-         padding: const EdgeInsets.symmetric(
+        padding: const EdgeInsets.symmetric(
           horizontal: 16,
           vertical: 10,
         ),
@@ -90,13 +90,72 @@ class _CartState extends State<Cart> {
       );
     }
 
+    Widget titleCartList() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20.0),
+      child: Text(
+        'Daftar Item',
+        style: primaryTextStyle.copyWith(
+          fontSize: 16,
+          fontWeight: semiBold,
+        ),
+      ),
+    );
+  }
+
+    Widget cartIsEmpty() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin*2.5,
+          right: defaultMargin,
+          left: defaultMargin,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons/icon_empty_cart.png',
+              width: 80,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Opss!',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            Text(
+              'Keranjang anda Kosong',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Silahkan pilih menu',
+              maxLines: 2,
+              style: secondaryTextStyle,
+            ),
+            Text(
+              'yang akan anda Pesan',
+              maxLines: 2,
+              style: secondaryTextStyle,
+            ),
+          ]
+        ),
+      );
+    }
+
     Widget content() {
       return ListView(
         padding: EdgeInsets.symmetric(
           horizontal: defaultMargin,
         ),
         children: [
-          customerInfo()
+          customerInfo(),
+          cartIsEmpty(),
         ],
       );
     }
